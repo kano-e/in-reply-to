a31092a8fa2a70b14693f0b71afce65d2d904899
Eri Takano <e-takano@feedforce.jp>
2016-06-28 20:16:02 +0900

    counter_cache を有効に

--- app/models/advice.rb
+++ app/models/advice.rb
@@ -1,5 +1,5 @@
 class Advice < ApplicationRecord
-  belongs_to :worry
-  belongs_to :in_reply_to, class_name: self
+  belongs_to :worry, counter_cache: :advices_count
+  belongs_to :in_reply_to, class_name: self, counter_cache: :replies_count
   has_many :replies, class_name: self, foreign_key: :in_reply_to_id
 end
