de524f5393802b91c01f5a3552e24126bedff2fb
Eri Takano <e-takano@feedforce.jp>
2016-06-28 22:21:56 +0900

    悩みがランダムに表示されるようにした

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -3,8 +3,8 @@ class MessageController < ApplicationController
     item = nil
 
     if Worry.open.count > 5
-      item = Worry.open.has_no_advices.take
-      item ||= Advice.open.has_no_replies.take
+      item = Worry.open.has_no_advices.order('RANDOM()').take
+      item ||= Advice.open.has_no_replies.order('RANDOM()').take
     end
 
     @message = Message.new(item)
