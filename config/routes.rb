433d72e3ef181d237dea6453abb47277097089ac
Eri Takano <e-takano@feedforce.jp>
2016-06-28 21:23:50 +0900

    fixup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :advices, only: [:index, :new, :show, :create]
-  resources :worries, only: [:new, :create]
+  resources :advices, only: [:new, :create]
+  resources :worries, only: [:index, :show, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'message#show'
