dcf94093688cd811a755e64e26008addce8f5cee
Eri Takano <e-takano@feedforce.jp>
2016-06-28 20:15:04 +0900

    counter_cache 用のカラムは default 0

--- db/migrate/20160628111343_add_column_advices_count_to_worries.rb
+++ db/migrate/20160628111343_add_column_advices_count_to_worries.rb
@@ -1,5 +1,5 @@
 class AddColumnAdvicesCountToWorries < ActiveRecord::Migration[5.0]
   def change
-    add_column :worries, :advices_count, :integer
+    add_column :worries, :advices_count, :integer, default: 0
   end
 end
--- db/migrate/20160628111410_add_column_replies_count_to_advices.rb
+++ db/migrate/20160628111410_add_column_replies_count_to_advices.rb
@@ -1,5 +1,5 @@
 class AddColumnRepliesCountToAdvices < ActiveRecord::Migration[5.0]
   def change
-    add_column :advices, :replies_count, :integer
+    add_column :advices, :replies_count, :integer, default: 0
   end
 end
