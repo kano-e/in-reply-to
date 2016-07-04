70ba18f64ddfa07f8eff898d5e51295b106bbb2b
Eri Takano <e-takano@feedforce.jp>
2016-07-04 23:11:12 +0900

    $ rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160628114702) do
+ActiveRecord::Schema.define(version: 20160704140737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "accounts", force: :cascade do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.json     "credentials"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "advices", force: :cascade do |t|
     t.text     "detail"
