623bb796fb365ff340f74408550675616ca40da6
Eri Takano <e-takano@feedforce.jp>
2016-07-07 22:47:29 +0900

    `&.` をやめた

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -3,8 +3,8 @@ class MessageController < ApplicationController
     items = [nil, nil]
 
     if Worry.open.count >= 3
-      items += Worry.open.has_no_advices.not_mine(signed_in_user&.id).order('RANDOM()').limit(3).to_a
-      items += Advice.open.has_no_replies.not_mine(signed_in_user&.id).order('RANDOM()').limit(5).to_a
+      items += Worry.open.has_no_advices.not_mine(signed_in_user.try(:id)).order('RANDOM()').limit(3).to_a
+      items += Advice.open.has_no_replies.not_mine(signed_in_user.try(:id)).order('RANDOM()').limit(5).to_a
     end
 
     @message = Message.new(items.shuffle.first)
