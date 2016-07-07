e33b172db75285967bcc5f6db635669d98fd9a6b
Eri Takano <e-takano@feedforce.jp>
2016-07-07 20:23:31 +0900

    Add relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :rememberable, :trackable
+
+  has_many :accounts
 end
