4fe9e7b8b224fd65affc960f6bd1ad70adb6bd22
Eri Takano <e-takano@feedforce.jp>
2016-07-07 21:13:38 +0900

    Add relation to user

--- app/models/advice.rb
+++ app/models/advice.rb
@@ -1,4 +1,5 @@
 class Advice < ApplicationRecord
+  belongs_to :user
   belongs_to :worry, counter_cache: :advices_count
   belongs_to :in_reply_to, class_name: self, optional: true, counter_cache: :replies_count
   has_many :replies, class_name: self, foreign_key: :in_reply_to_id
--- app/models/worry.rb
+++ app/models/worry.rb
@@ -1,4 +1,6 @@
 class Worry < ApplicationRecord
+  belongs_to :user
+
   has_many :advices
 
   scope :has_no_advices, -> { where(advices_count: 0) }
